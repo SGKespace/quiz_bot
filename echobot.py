c282761afe7c500783a01fa94dfcae770fcf162f
SGK <55636018+SGKespace@users.noreply.github.com>
2023-08-01 14:25:46 +0400

    Контрольная точка перед внесением сильных правок

--- echobot.py
+++ echobot.py
@@ -1,46 +1,87 @@
 import logging
 from dotenv import load_dotenv
 import os
+import redis
 
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
+    CallbackContext, ConversationHandler
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+ANSWERS = range(1)
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+    text = 'Привет! Я бот для викторин. Не хочешь играть сразу жми Сдаться'
+    keyboard = [
+        ["Новый вопрос", "Сдаться"],
+                        ['Мой счет']
+    ]
+    markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+    update.message.reply_text(text=text, reply_markup=markup)
+
+    return ANSWERS
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
+def cancel(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    update.message.reply_text('Напугался? Может попробуем еще раз?  Жми /start', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def question():
+    return ANSWERS
+
+
+def fail(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    update.message.reply_text('Напугался? Может попробуем еще раз?  Жми /start', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+def score():
+    return ANSWERS
+
+def answer ():
+    return ANSWERS
+
 
 
 def main() -> None:
     load_dotenv()
     telegram_token = os.environ["TELEGRAM_TOKEN"]
+    chat_id = os.environ['TG_CHAT_ID']
+
+    redis_host = os.environ["REDIS_HOST"]
+    redis_port = os.environ["REDIS_PORT"]
+    redis_password = os.environ["REDIS_PASSWORD"]
+
+    r = redis.Redis(host=redis_host, port=redis_port, db=0, password=redis_password)
+    r.set(f'{chat_id}', 'bar')
+    r.get('foo')
 
     updater = Updater(telegram_token)
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            ANSWERS: [
+                MessageHandler(Filters.text("Новый вопрос"), question),
+                MessageHandler(Filters.text("Сдаться"), fail),
+                MessageHandler(Filters.text("Мой счет"), score),
+                MessageHandler(Filters.text & ~Filters.command, answer)
+            ],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],)
+
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-
+    dispatcher.add_handler(conv_handler)
     updater.start_polling()
     updater.idle()
+
+
 
 
 if __name__ == '__main__':
